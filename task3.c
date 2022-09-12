7599ab330f5ebcef42fa2b985e846abb918834bc
rafvel17 <rafvel17@ya.ru>
2022-09-12 04:04:35 +0300

    comments to task3.c added run3.sh created

--- task3.c
+++ task3.c
@@ -2,22 +2,20 @@
 #include "omp.h"
 
 int main () {
-    int N = 10;
+    int N = 100000;
     int a[N];
     for (int i = 0; i < N; i++) {
         a[i] = i;
     }
     int N_max = omp_get_max_threads();
-    int edge = 0;
-    int wind[3];
+    int edge = 0; // to fix elements of a[] which are rewritten in a thread but also needed in another one
+    int wind[3]; //the auxiliary array to store previous elements and perform calculations with them to change a[] 
     #pragma omp parallel shared(a) firstprivate(N_max,edge,wind) 
     {
         int pid = omp_get_thread_num();
-        int StartPoint = (int)(1.0 * pid / N_max * N); 
-        int EndPoint = (int)(1.0 * (pid+1) / N_max * N);
-        if (pid == N_max - 1) {
-            EndPoint = N;
-        }
+        int StartPoint = (int)(1.0 * pid / N_max * N); //starting point in a for-cycle of a thread
+        int EndPoint = (int)(1.0 * (pid+1) / N_max * N); //ending point in a for-cycle of a thread
+       
             if (StartPoint == 0) {
             wind[0] = a[StartPoint];
             wind[1] = a[StartPoint];
@@ -36,16 +34,16 @@ int main () {
                 wind[2] = a[StartPoint+1];
                 edge = a[EndPoint-1];
             }
-        #pragma omp barrier
+        #pragma omp barrier //to form all needed winds and edges in all threads before the main actions
         for (int i = StartPoint; i < EndPoint; i++) {
-            if (i == N-1) {
+            if (i == N-1) {                                //getting a[N-1]
                 a[i] = (int)(a[i]*a[i]*wind[0] / 3.0);
-            }
-            else{
+            }                                            
+            else{                                          
             a[i] = wind[0] * wind[1] * wind[2] / 3;
             wind[0] = wind[1];
             wind[1] = wind[2];
-            if (i == EndPoint - 2) {
+            if (i == EndPoint - 2) {               //using edge because other thread could have changed a[EndPoint] before
                 wind[2] = edge;
             }
             else {
@@ -54,9 +52,9 @@ int main () {
             }
         }
     }
-     for (int i = 0; i < N; i++) {
-         printf("%d\n", a[i]);
-     }
+    //  for (int i = 0; i < 10; i++) {
+    //      printf("%d\n", a[i]);
+    //  }
     
     return 0;
 }
